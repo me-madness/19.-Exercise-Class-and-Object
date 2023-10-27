ed5e645d15273db0768148d9ff65ae20fce7e289
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-27 20:34:47 +0200

    second task by the lecture

--- 01. storage.py
+++ 01. storage.py
@@ -13,3 +13,20 @@ class Storage:
  
     def get_products(self):
         return Storage.storage
+    
+# Second task by the lecture
+
+class Storage:
+    storage = []
+ 
+    def __init__(self, capacity):
+        self.capacity = capacity
+ 
+    def add_product(self, product: str):
+        if self.capacity > len(Storage.storage):  # There is free space
+            Storage.storage.append(product)
+ 
+    def get_products(self):
+        return Storage.storage
+    
+        
