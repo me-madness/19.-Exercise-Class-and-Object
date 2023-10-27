6e4438398bfe7e4c720df3c930c1b450c1b9c1fc
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-27 20:37:36 +0200

    02. second way it's from me

--- 02. weapon.py
+++ 02. weapon.py
@@ -1,2 +1,17 @@
 # First way by the lecture
 
+class Weapon:
+    def __init__(self, bullets: int):
+        self.bullets = bullets
+ 
+    def shoot(self):
+        if self.bullets > 0:
+            self.bullets -= 1
+            return "shooting..."
+        return "no bullets left"
+ 
+    def __repr__(self):
+        return f"Remaining bullets: {self.bullets}"
+    
+
+# Second way by me    
