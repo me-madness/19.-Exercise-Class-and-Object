af3af82c9edcc091f0f292d0b9b6eeac4f2f6d61
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-29 14:39:14 +0100

    two options from the lecture

--- 06. inventory.py
+++ 06. inventory.py
@@ -22,3 +22,25 @@ class Inventory:
 
 # Second task also from the lecture    
 
+class Inventory:
+ 
+    def __init__(self, __capacity: int):
+        self.__capacity = __capacity
+        self.items = []
+ 
+    def add_item(self, item: str):
+        if self.__capacity > len(self.items):
+            self.items.append(item)
+        else:
+            return "not enough room in the inventory"
+ 
+    def get_capacity(self):
+        return len(self.items)
+ 
+    def __repr__(self):
+        return f"Items: {', '.join(self.items)}.\nCapacity left: {self.__capacity - len(self.items)}"
+    
+    
+# Third task from me
+
+    
